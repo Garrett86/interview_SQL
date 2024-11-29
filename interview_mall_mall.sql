4d8cba4f06bd72d9ad3d2a73068b13b75b5c30e7
Garrett86 <89382061+Garrett86@users.noreply.github.com>
2024-11-29 20:30:23 +0800

    Update interview_mall_mall.sql

--- interview_mall_mall.sql
+++ interview_mall_mall.sql
@@ -29,7 +29,7 @@ CREATE TABLE `mall` (
   `saleCount` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `breadCount` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `mall` (
 
 LOCK TABLES `mall` WRITE;
 /*!40000 ALTER TABLE `mall` DISABLE KEYS */;
-INSERT INTO `mall` VALUES (1,'NiKI球鞋','1200','300','4000'),(2,'adidas球鞋','2400','250','940'),(3,'FILA球鞋','3200','140','500'),(4,'記事本','25','3000','1200'),(18,'瑪德蓮','45','6454','4545'),(19,'adidas球鞋','1290','5555','66666'),(20,'IRIS OHYAMA鑽石不沾鍋具','4999','2300','545454'),(21,'Neoflam鍋具','5990','434334','3434343'),(22,'Snow Peak雪諾必克鍋具','6999','5454','545454'),(23,'New Balance球鞋','2930','34343','3434343'),(24,'Tefal不沾鍋具','5990','3434','34343');
+INSERT INTO `mall` VALUES (1,'NiKI球鞋','1200','300','4000'),(2,'adidas球鞋','2400','250','940'),(3,'FILA球鞋','3200','140','500'),(4,'記事本','25','3000','1200'),(19,'adidas球鞋','1290','5555','66666'),(20,'IRIS OHYAMA鑽石不沾鍋具','4999','2300','545454'),(21,'Neoflam鍋具','5990','434334','3434343'),(22,'Snow Peak雪諾必克鍋具','6999','5454','545454'),(23,'New Balance球鞋','2930','34343','3434343'),(24,'Tefal不沾鍋具','5990','3434','34343'),(25,'戚風蛋糕','499','4545','6766'),(26,'鳳梨酥','244','5454','45454'),(27,'Nintendo 任天堂超級瑪利歐派對','499','4545','45454'),(28,'海綿蛋糕','399','3434','3434');
 /*!40000 ALTER TABLE `mall` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-29 19:46:38
+-- Dump completed on 2024-11-29 20:30:07
